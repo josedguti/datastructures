343868b965980498d88cff64cf44eadd591a795f
Jose Gutierrez <josepmind@gmail.com>
2021-11-14 14:48:48 +0100

    finished js and data sctructures cert

--- projects.js
+++ projects.js
@@ -154,6 +154,98 @@ function telephoneCheck(str) {
 
 telephoneCheck("555-555-5555");
 
-//
+/* Design a cash register drawer function checkCashRegister() that accepts purchase price as the first argument (price), payment as the second argument (cash), and cash-in-drawer (cid) as the third argument.
 
+cid is a 2D array listing available currency.
+
+The checkCashRegister() function should always return an object with a status key and a change key.
+
+Return {status: "INSUFFICIENT_FUNDS", change: []} if cash-in-drawer is less than the change due, or if you cannot return the exact change.
+
+Return {status: "CLOSED", change: [...]} with cash-in-drawer as the value for the key change if it is equal to the change due.
+
+Otherwise, return {status: "OPEN", change: [...]}, with the change due in coins and bills, sorted in highest to lowest order, as the value of the change key.
+
+Currency Unit	Amount
+Penny	$0.01 (PENNY)
+Nickel	$0.05 (NICKEL)
+Dime	$0.1 (DIME)
+Quarter	$0.25 (QUARTER)
+Dollar	$1 (ONE)
+Five Dollars	$5 (FIVE)
+Ten Dollars	$10 (TEN)
+Twenty Dollars	$20 (TWENTY)
+One-hundred Dollars	$100 (ONE HUNDRED)
+See below for an example of a cash-in-drawer array:
+
+[
+  ["PENNY", 1.01],
+  ["NICKEL", 2.05],
+  ["DIME", 3.1],
+  ["QUARTER", 4.25],
+  ["ONE", 90],
+  ["FIVE", 55],
+  ["TEN", 20],
+  ["TWENTY", 60],
+  ["ONE HUNDRED", 100]
+] */
+
+const moneyMap = {
+  'ONE HUNDRED': 100,
+  TWENTY: 20,
+  TEN: 10,
+  FIVE: 5,
+  ONE: 1,
+  QUARTER: 0.25,
+  DIME: 0.1,
+  NICKEL: 0.05,
+  PENNY: 0.01,
+};
+
+const getTotalCid = (cid) =>
+  cid.reduce((total, [unit, amount]) => total + amount, 0);
+
+const safelyRoundMoney = (amount) => Math.round(amount * 100) / 100;
+
+const checkCashRegister = (price, cash, cid) => {
+  const change = [];
+  let changeDue = cash - price;
+
+  if (changeDue === getTotalCid(cid)) {
+    return {
+      status: 'CLOSED',
+      change: cid,
+    };
+  }
+
+  [...cid].reverse().forEach(([unit, amount]) => {
+    const unitValue = moneyMap[unit];
+
+    if (unitValue > changeDue) {
+      return;
+    }
+
+    let unitsTaken = 0;
+
+    while (changeDue >= unitValue && amount > 0) {
+      changeDue = safelyRoundMoney(changeDue - unitValue);
+      amount = safelyRoundMoney(amount - unitValue);
+      unitsTaken++;
+    }
+
+    change.push([unit, unitsTaken * unitValue]);
+  });
+
+  if (changeDue > 0) {
+    return {
+      status: 'INSUFFICIENT_FUNDS',
+      change: [],
+    };
+  }
+
+  return {
+    status: 'OPEN',
+    change,
+  };
+};
 
